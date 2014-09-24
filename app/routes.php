eb3a03aa788f0857491cb561025d533e45f2c0fe
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2014-09-24 16:41:35 +0200

    Clean directory. ready to go

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,4 @@
 <?php
-use Faker\Factory as Faker;
-use Acme\User\User;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,28 +11,7 @@ use Acme\User\User;
 */
 
 Route::resource('users', 'UsersController');
-Route::resource('profiles', 'ProfilesController');
 
 Route::get('/', function () {
 
-    // return View::make('hello')->with('name',$jalal);
-    //
-    // Eloquent::unguard();
-        $faker = Faker::create();
-
-    foreach (range(1, 10) as $index) {
-        User::create([
-            'firstname' => $faker->unique()->sentence(3),
-            'lastname'  => $faker->unique()->firstname(),
-            'password'  => $faker->unique()->firstname(),
-            'email'     => $faker->unique()->firstname(),
-
-        ]);
-    }
-    $jalal =DB::table('users')->get();
-
-    return $jalal;
-    // return $faker->unique()->sentence(30);
-
-    // return View::make('test');
 });
