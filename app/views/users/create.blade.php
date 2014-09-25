ba108b412e326c47c16bb96071b6aede24fa009c
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2014-09-25 03:55:20 +0200

    First form careted to register new user

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -7,11 +7,31 @@
 
 
 @section('test-content')
-    {{BootstrapForm::openInline()}}
-
-    {{BootstrapForm::text('username')}}
-    {{BootstrapForm::text('password')}}
-
-
+    {{ Form::open(array('route' => 'users.store','role' => 'form','class' => 'form')) }}
+        <div class="form-group">
+            {{ Form::label('firstname','First Name: ')}}
+            {{ Form::text('firstname',null, array('class'       => 'form-control',
+                                                'id'          => 'firstname',
+                                                'placeholder' => 'First Name'  ))}}
+        </div>
+        <div class="form-group">
+            {{ Form::label('lastname','Last Name: ')}}
+            {{ Form::text('lastname',null, array('class'       => 'form-control',
+                                                'id'          => 'lastname',
+                                                'placeholder' => 'Last Name'  ))}}
+        </div>
+        <div class="form-group">
+            {{ Form::label('email','Email: ')}}
+            {{ Form::text('email',null, array('class'       => 'form-control',
+                                                'id'          => 'email',
+                                                'placeholder' => 'Email'  ))}}
+        </div>
+        <div class="form-group">
+            {{ Form::label('password','Password: ')}}
+            {{ Form::password('password', array('class'       => 'form-control',
+                                                'id'          => 'password',
+                                                 ))}}
+        </div>
+    {{ Form::close() }}
 
 @stop
