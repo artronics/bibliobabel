c24ddc993138b74923eb954751cc363d5ffae7a7
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2014-11-20 15:22:42 +0100

    add username to user table migration

--- app/database/migrations/2014_09_07_224222_create_users_tabel.php
+++ app/database/migrations/2014_09_07_224222_create_users_tabel.php
@@ -16,6 +16,7 @@ class CreateUsersTabel extends Migration
             $table->increments('id');
             $table->string('name',40);
             $table->string('email',40)->unique();
+            $table->string('username',40)->unique();
             $table->string('password',64);
             $table->string('remember_token',100)->nullabe();
             $table->timestamps();
