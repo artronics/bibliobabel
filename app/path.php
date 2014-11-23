420399974afc3954a051015e41a039be1cb3918f
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2014-11-23 11:37:24 +0000

    this file contains path constants to models and other classes. this gonna helpful during refrencing classes inside strings

---
+++ app/path.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * I'm using these constant for easy class refrences
+ * where needed as strings
+ */
+const ART_USER ='Artronics\User\User';
+const ART_PROFILE ='Artronics\Profile\Profile';
