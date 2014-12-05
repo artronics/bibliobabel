3319f919db8038397fbd1142683cd7403a93cbae
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2014-12-05 10:53:54 +0000

    login form is changed to match foundation

--- app/views/include/forms/login.blade.php
+++ app/views/include/forms/login.blade.php
@@ -1,18 +1,12 @@
 {{ Form::open(array('route' => 'sessions.store','role' => 'form')) }}
     <div class="row">
         <div class="large-12 columns">
-            {{--<div>--}}
-                <label>Email:
-                    {{ Form::text('email',null, array('placeholder' => 'Email'  ))}}
-                </label>
-            {{--</div>--}}
-            <label>Password:
-                {{ Form::password('password', array('placeholder' => 'Password'))}}
-            </label>
 
-            {{Form::alert('salam')}}
+            {{ Form::input('email', 'email',null, array('placeholder' => 'Email'  ),'Email:')}}
 
-            {{ Form::submit('Sign in', array('class'       => 'button radius tiny'))}}
+            {{ Form::input('password', 'password', null, array('placeholder' => 'Password'),'Password:')}}
+
+            {{ Form::submit('Sign in', array('class' => 'button radius tiny'))}}
         </div>
     </div>
 {{ Form::close() }}
